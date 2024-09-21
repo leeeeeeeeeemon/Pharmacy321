40c0a1dd3b02cdbb3ba3e1f2b0f2179717ad7b88
Марсель Мубаракшин <marselmubaraksin231@mail.ru>
2024-09-21 10:38:03 +0300

    Создана авторизация и прописаны стили

--- Pharmacy321/MainWindow.xaml.cs
+++ Pharmacy321/MainWindow.xaml.cs
@@ -16,9 +16,71 @@ namespace Pharmacy321
     /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly string adminPassword = "admin123";//админ
+        private readonly string employeePassword = "emp123";//сотрудник
+        private readonly string accountantPassword = "acc123";//бухгалтер
+        private readonly string doctorPassword = "doc123";//врач
         public MainWindow()
         {
             InitializeComponent();
+            RoleComboBox.ItemsSource = new string[] { "Администратор", "Сотрудник", "Бухгалтер", "Специалист" };
+            RoleComboBox.SelectedIndex = 0;
+        }
+        private void LoginButton_Click(object sender, RoutedEventArgs e)
+        {
+            string selectedRole = RoleComboBox.SelectedItem.ToString();
+            string enteredPassword = PasswordBox.Password;
+
+            // Проверка пароля для каждой роли
+            if (selectedRole == "Администратор" && enteredPassword == adminPassword)
+            {
+                OpenAdminWindow();
+            }
+            else if (selectedRole == "Сотрудник" && enteredPassword == employeePassword)
+            {
+                OpenEmployeeWindow();
+            }
+            else if (selectedRole == "Бухгалтер" && enteredPassword == accountantPassword)
+            {
+                OpenAccountantWindow();
+            }
+            else if (selectedRole == "Специалист" && enteredPassword == doctorPassword)
+            {
+                OpenDoctorWindow();
+            }
+            else
+            {
+                MessageBox.Show("Неверный пароль!", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        // Методы для открытия окон для каждой роли
+        private void OpenAdminWindow()
+        {
+            AdminWindow adminWindow = new AdminWindow();
+            adminWindow.Show();
+            this.Close();
+        }
+
+        private void OpenEmployeeWindow()
+        {
+            EmployeeWindow employeeWindow = new EmployeeWindow();
+            employeeWindow.Show();
+            this.Close();
+        }
+
+        private void OpenAccountantWindow()
+        {
+            AccountantWindow accountantWindow = new AccountantWindow();
+            accountantWindow.Show();
+            this.Close();
+        }
+
+        private void OpenDoctorWindow()
+        {
+            DoctorWindow doctorWindow = new DoctorWindow();
+            doctorWindow.Show();
+            this.Close();
         }
     }
 }
