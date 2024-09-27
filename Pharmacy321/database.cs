717fc5e00921e9f7eff5c7378a82cbe2791c0c46
Марсель Мубаракшин <marselmubaraksin231@mail.ru>
2024-09-27 10:33:41 +0300

    привязал БД к WPF

---
+++ Pharmacy321/database.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy321
+{
+    public class Database
+    {
+        private string connectionString;
+
+        public Database()
+        {
+            connectionString = ConfigurationManager.ConnectionStrings["PharmacyDB"].ConnectionString;
+        }
+
+        public DataTable GetClients()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "SELECT * FROM Klient";
+                SqlDataAdapter adapter = new SqlDataAdapter(query, conn);
+                DataTable dataTable = new DataTable();
+                adapter.Fill(dataTable);
+                return dataTable;
+            }
+        }
+    }
+}
