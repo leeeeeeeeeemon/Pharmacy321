26d104a129dbeeccc3b3bc0b8d61f7504252dbcc
212201 <212201@1220-010-22>
2024-10-04 09:23:17 +0300

    загрузка бд, окно администратора

---
+++ Pharmacy/Data/DBManager.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pharmacy.Data
+{
+    public class DBManager
+    {
+        private static Pharmacy_MDKEntities _context = new Pharmacy_MDKEntities(); 
+        public static Pharmacy_MDKEntities GetContext()
+        {
+            if (_context == null)
+                _context = new Pharmacy_MDKEntities(); return _context;
+        }
+        public static bool UpdateDatabase()
+        {
+            try
+            {
+                _context.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+        public static List<Sotrydniki> GetStaff()
+        {
+            return _context.Sotrydniki.ToList();
+        }
+    }
+}
---
+++ Pharmacy/Windows/AuthWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Pharmacy.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для AuthWndow.xaml
+    /// </summary>
+    public partial class AuthWindow : Window
+    {
+        public AuthWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Btn_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Login_CB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+    }
+}
